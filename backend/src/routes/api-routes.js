0d9d9afc81060e8f631686452009029ea2bfb2ee
Scarlett7-code <cs.zsuzsi333@gmail.com>
2025-01-04 15:25:58 +0100

    Create products API endpoints

--- backend/src/routes/api-routes.js
+++ backend/src/routes/api-routes.js
@@ -1,9 +1,13 @@
 import express from "express";
 import categoriesRoutes from "./categories-routes.js";
+import productsRoutes from "./products-routes.js"
+
 
 const router = express.Router();
 
 router.use("/categories", categoriesRoutes);
+router.use("/products", productsRoutes);
+
 
 
 export default router;
---
+++ backend/src/routes/products-routes.js
@@ -0,0 +1,13 @@
+import express from "express";
+import productsController from "../controllers/products-controller.js";
+
+const router = express.Router();
+
+router.get("/", productsController.getAllProducts);
+router.get("/category/:categoryId", productsController.getAllProductsByCategory);
+router.get("/:id", productsController.getProductById);
+router.post("/", productsController.createProduct);
+router.put("/:id", productsController.updateProduct);
+router.delete("/:id", productsController.destroyProduct);
+
+export default router;
