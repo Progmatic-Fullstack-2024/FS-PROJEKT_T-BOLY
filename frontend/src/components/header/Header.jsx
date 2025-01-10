8f22abd3e3952c7edacd9130d7b78e21e0beadb1
rekapuskas <166331809+rekapuskas@users.noreply.github.com>
2025-01-10 11:06:02 +0100

    modify headers style

--- frontend/src/components/header/Header.jsx
+++ frontend/src/components/header/Header.jsx
@@ -16,14 +16,15 @@ export default function Header() {
           <img src={LogoOrange} alt="" className="w-18 h-10" />
           <img src={LogoText} alt="" className="w-18 h-4" />
         </div>
-
-        <Nav />
-        <div className="flex items-center ">
-          <button>
-            <FiShoppingCart className='m-2'/>
-          </button>
-          <Searchbar />
-        </div>
+       
+          <Nav className="order-2 sm:order-1 " />
+          <div className="flex  items-center order-1 sm:order-2">
+            <button>
+              <FiShoppingCart className="m-2" />
+            </button>
+            <Searchbar />
+          </div>
+        
       </div>
     </header>
   );
