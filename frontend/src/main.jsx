7f90654db1740b89e0587993a4d87d41533cbc84
vjanos1030 <vantaljanos1030@gmail.com>
2025-01-13 19:43:17 +0100

    Create Admin Dashboard

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import App from './App';
+import ProductsTable from './components/admin/ProductsTable.jsx';
 import ProductsByCategory from './components/products/ProductsByCategory.jsx';
 import './index.css';
 import { AuthProvider } from './contexts/AuthContext.jsx';
 import About from './pages/About.jsx';
+import AdminLayout from './pages/AdminLayout.jsx';
 import Contacts from './pages/Contact.jsx';
 import Homepage from './pages/Homepage';
 
@@ -30,6 +32,16 @@ const router = createBrowserRouter([
       {
         path: '/products/category/:categoryId',
         element: <ProductsByCategory />,
+      },
+    ],
+  },
+  {
+    path: '/admin',
+    element: <AdminLayout />,
+    children: [
+      {
+        path: '/admin/products',
+        element: <ProductsTable />,
       },
     ],
   },
