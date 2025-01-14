0181b70fc29d95ff1897f9fd52b5f35ce3c50d4e
rekapuskas <166331809+rekapuskas@users.noreply.github.com>
2025-01-14 20:40:23 +0100

    fix the problem while rendering after update

--- backend/src/controllers/user-controller.js
+++ backend/src/controllers/user-controller.js
@@ -1,19 +1,8 @@
 import userService from "../services/user-service.js";
 
 const updateUser = async (req, res, next) => {
-  const { id } = req.params;
-  const {
-    firstName,
-    lastName,
-    email,
-    username,
-    birthDate,
-    adress,
-    billingAdress,
-    profilePictureUrl,
-  } = req.body;
-  try {
-    const updatedUser = await userService.updateUser(id, {
+    const { id } = req.params;
+    const {
       firstName,
       lastName,
       email,
@@ -22,11 +11,25 @@ const updateUser = async (req, res, next) => {
       adress,
       billingAdress,
       profilePictureUrl,
-    });
-    res.status(200).json(updatedUser);
-  } catch (error) {
-    next(error);
-  }
-};
+    } = req.body;
+  
+    try {
+      const { updatedUser, token } = await userService.updateUser(id, {
+        firstName,
+        lastName,
+        email,
+        username,
+        birthDate,
+        adress,
+        billingAdress,
+        profilePictureUrl,
+      });
+  
+    
+      res.status(200).json({ token, updatedUser });
+    } catch (error) {
+      next(error);
+    }
+  };
 
 export default { updateUser };
--- backend/src/services/user-service.js
+++ backend/src/services/user-service.js
@@ -1,5 +1,7 @@
 import prisma from "../models/prismaClient.js";
 import HttpError from "../utils/HttpError.js";
+import jwt from "jsonwebtoken"
+import { JWT_SECRET } from "../constants/constants.js";
 
 const updateUser = async (id, userData) => {
   const user = await prisma.user.findUnique({
@@ -13,7 +15,22 @@ const updateUser = async (id, userData) => {
     data: userData, 
   });
 
-  return updatedUser;
+  const token = jwt.sign(
+    {  id: updatedUser.id,
+        email: updatedUser.email,
+        role: updatedUser.role,
+        username: updatedUser.username,
+        firstName: updatedUser.firstName,
+        lastName: updatedUser.lastName,
+        birthDate: updatedUser.birthDate,
+        adress: updatedUser.adress,
+        billingAdress: updatedUser.billingAdress,
+        profilePictureUrl: updatedUser.profilePictureUrl, },
+    JWT_SECRET, 
+    { expiresIn: '1h' } 
+  );
+
+  return { token, updatedUser};
 };
 
 export default { updateUser };
