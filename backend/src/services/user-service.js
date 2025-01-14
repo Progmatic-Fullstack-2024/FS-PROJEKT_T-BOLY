d1d4d88f6922bb350db2e3afa707c3775a7a0ba3
rekapuskas <166331809+rekapuskas@users.noreply.github.com>
2025-01-14 20:07:45 +0100

    add user service

--- backend/src/services/user-service.js
+++ backend/src/services/user-service.js
@@ -2,14 +2,17 @@ import prisma from "../models/prismaClient.js";
 import HttpError from "../utils/HttpError.js";
 
 const updateUser = async (id, userData) => {
-  const user = prisma.user.findUnique({
+  const user = await prisma.user.findUnique({
     where: { id },
   });
+  
   if (!user) throw new HttpError("User not found", 404);
-  const updatedUser = await prisma.product.update({
+
+  const updatedUser = await prisma.user.update({
     where: { id },
-    data: userData,
+    data: userData, 
   });
+
   return updatedUser;
 };
 
