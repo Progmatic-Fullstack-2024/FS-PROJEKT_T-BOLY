3f35bec17a7f498ed03ca8fea7578e36a08f6981
Dániel Zoltán <102678885+szimplexum@users.noreply.github.com>
2025-01-18 18:17:31 +0100

    Create Cloudinary Connection - Image Upload

---
+++ backend/src/middlewares/multer.js
@@ -0,0 +1,20 @@
+import multer from "multer";
+
+export const uploadToOwnServer = multer({
+  storage: multer.diskStorage({
+    destination: "upload/",
+    filename: (req, file, cb) => {
+      cb(null, file.originalname);
+    },
+  }),
+  limits: { fileSize: 5000000 },
+});
+
+export const upload = multer({
+  storage: multer.diskStorage({
+    filename: (req, file, cb) => {
+      cb(null, file.originalname);
+    },
+  }),
+  limits: { fileSize: 5000000 },
+});
