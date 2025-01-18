20ff120454b6f1e5c27e5f519c0a4468fe7d2868
Dániel Zoltán <102678885+szimplexum@users.noreply.github.com>
2025-01-18 18:43:30 +0100

    Fix missing import

--- frontend/src/components/admin/ProductsTable.jsx
+++ frontend/src/components/admin/ProductsTable.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { BsDownload } from 'react-icons/bs';
 import { toast } from 'react-toastify';
 
 import AddNewProduct from './CreateProductByAdmin.jsx';
