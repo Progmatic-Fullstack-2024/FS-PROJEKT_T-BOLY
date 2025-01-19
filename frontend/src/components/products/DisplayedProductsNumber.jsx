f3f823ed649e1523dc22b3c528f35201bf0e3996
Scarlett7-code <cs.zsuzsi333@gmail.com>
2025-01-19 23:44:28 +0100

    Update pagination

--- frontend/src/components/products/DisplayedProductsNumber.jsx
+++ frontend/src/components/products/DisplayedProductsNumber.jsx
@@ -1,11 +1,9 @@
-
-
-export default function DisplayedProductsNumber({pageNumber, totalProducts}) {
+export default function DisplayedProductsNumber({limit, pageNumber, totalProducts}) {
   return (
     <div>
       <h1>
-        Showing {Math.min((pageNumber - 1) * 9 + 1, totalProducts)} -{' '}
-        {Math.min(pageNumber * 9, totalProducts)} of {totalProducts} results
+        Showing {Math.min((pageNumber - 1) * limit + 1, totalProducts)} -{' '}
+        {Math.min(pageNumber * limit, totalProducts)} of {totalProducts} results
       </h1>
     </div>
   );
--- frontend/src/components/products/Pagination.jsx
+++ frontend/src/components/products/Pagination.jsx
@@ -1,23 +1,25 @@
 import React from 'react';
 import { MdKeyboardArrowRight, MdKeyboardArrowLeft } from 'react-icons/md';
 
-export default function Pagination({ pageNumber, setPageNumber, getPageNumbers, totalPages }) {
+import getPageNumbers from '../../utils/getPageNumbers';
+
+export default function Pagination({ pageNumber, setPageNumber, totalPages }) {
   return (
     <div className="flex mt-16 justify-center mr-44">
       <div className="flex items-center h-12 gap-2">
         <button
           type="button"
           onClick={() => setPageNumber(Math.max(1, pageNumber - 1))}
-          disabled={pageNumber === 1}
+          disabled={pageNumber <= 1}
         >
           <div
-            className={`flex items-center justify-center px-2 h-10 ms-0 leading-tight text-2xl text-gray-900 bg-white border border-1 border-gray-400 rounded-full ${pageNumber === 1 ? 'cursor-not-allowed text-gray-300 border-gray-100' : 'hover:text-primary hover:border-primary'}`}
+            className={`flex items-center justify-center px-2 h-10 ms-0 leading-tight text-2xl text-gray-900 bg-white border border-1 border-gray-400 rounded-full ${pageNumber <= 1 ? 'cursor-not-allowed text-gray-100 border-gray-100' : 'hover:text-primary hover:border-primary'}`}
           >
             <div className="sr-only">Previous</div>
             <MdKeyboardArrowLeft />
           </div>
         </button>
-        {getPageNumbers().map((page) => (
+        {getPageNumbers(pageNumber, totalPages).map((page) => (
           <div key={page}>
             <button
               type="button"
@@ -31,10 +33,10 @@ export default function Pagination({ pageNumber, setPageNumber, getPageNumbers, 
         <button
           type="button"
           onClick={() => setPageNumber(Math.min(totalPages, pageNumber + 1))}
-          disabled={pageNumber === totalPages}
+          disabled={pageNumber >= totalPages || pageNumber <= 0}
         >
           <div
-            className={`flex items-center justify-center px-2 h-10 ms-0 leading-tight text-2xl text-gray-900 bg-white border border-1 border-gray-400 rounded-full ${pageNumber === totalPages ? 'cursor-not-allowed text-gray-300 border-gray-100' : 'hover:text-primary hover:border-primary'}`}
+            className={`flex items-center justify-center px-2 h-10 ms-0 leading-tight text-2xl text-gray-900 bg-white border border-1 border-gray-400 rounded-full ${pageNumber >= totalPages || pageNumber <= 0 ? 'cursor-not-allowed text-gray-100 border-gray-100' : 'hover:text-primary hover:border-primary'}`}
           >
             <div className="sr-only">Next</div>
             <MdKeyboardArrowRight />
