e257f56e681b9ac2e4b1fe9c7d8fc57ba60d1607
Scarlett7-code <cs.zsuzsi333@gmail.com>
2025-01-22 00:19:53 +0100

    Fix filter by price slider and add clear button to filters

--- frontend/src/components/products/FilterByAge.jsx
+++ frontend/src/components/products/FilterByAge.jsx
@@ -2,7 +2,7 @@ import RangeSlider from 'react-range-slider-input';
 import 'react-range-slider-input/dist/style.css';
 import './slider.css';
 
-export default function FilterByPrice({minAge, maxAge, setMaxAge, setMinAge, handleFilterByAge}) {
+export default function FilterByPrice({minAge, maxAge, setMaxAge, setMinAge, handleFilterByAge, handleClearFilterByAge}) {
   return (
     <div className="flex flex-col gap-2 p-6 border-2 rounded-lg mb-10">
       <h3 className="text-xl mb-6">Filter by Age</h3>
@@ -20,9 +20,16 @@ export default function FilterByPrice({minAge, maxAge, setMaxAge, setMinAge, han
         <div>{minAge}</div>
         <div>{maxAge}</div>
       </div>
-      <div className="flex justify-end">
+      <div className="flex justify-between">
+      <button
+          className="w-28 rounded-xl border-2 p-2 hover:border-gray-900"
+          type="button"
+          onClick={handleClearFilterByAge}
+        >
+          Clear
+        </button>
         <button
-          className="w-28 rounded-xl bg-primary p-2 text-white  hover:text-black hover:font-semibold"
+          className="w-28 rounded-xl border-2 border-primary bg-primary p-2 text-white  hover:text-black hover:border-gray-900"
           type="button"
           onClick={handleFilterByAge}
         >
--- frontend/src/components/products/FilterByPrice.jsx
+++ frontend/src/components/products/FilterByPrice.jsx
@@ -1,14 +1,31 @@
+import { useEffect } from 'react';
 import RangeSlider from 'react-range-slider-input';
 import 'react-range-slider-input/dist/style.css';
 import './slider.css';
 
-export default function FilterByPrice({minPrice, maxPrice, setMaxPrice, setMinPrice, handleFilterByPrice}) {
+export default function FilterByPrice({
+  minPrice,
+  maxPrice,
+  setMaxPrice,
+  setMinPrice,
+  handleFilterByPrice,
+  priceRange,
+  handleClearFilterByPrice
+}) {
+  const min = priceRange.rangeMin;
+  const max = priceRange.rangeMax;
+
+  useEffect(() => {
+    setMinPrice(min);
+    setMaxPrice(max);
+  }, []);
+
   return (
     <div className="flex flex-col gap-2 p-6 border-2 rounded-lg mb-10">
       <h3 className="text-xl mb-6">Filter by price</h3>
       <RangeSlider
-        min={minPrice}
-        max={maxPrice}
+        min={min}
+        max={max}
         step={1}
         value={[minPrice, maxPrice]}
         onInput={(value) => {
@@ -20,9 +37,16 @@ export default function FilterByPrice({minPrice, maxPrice, setMaxPrice, setMinPr
         <div>€{minPrice}</div>
         <div>€{maxPrice}</div>
       </div>
-      <div className="flex justify-end">
+      <div className="flex justify-between">
+      <button
+          className="w-28 rounded-xl border-2 p-2 hover:border-gray-900"
+          type="button"
+          onClick={handleClearFilterByPrice}
+        >
+          Clear
+        </button>
         <button
-          className="w-28 rounded-xl bg-primary p-2 text-white  hover:text-black hover:font-semibold"
+          className="w-28 rounded-xl border-2 border-primary bg-primary p-2 text-white  hover:text-black hover:border-gray-900"
           type="button"
           onClick={handleFilterByPrice}
         >
