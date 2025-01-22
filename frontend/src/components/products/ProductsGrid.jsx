225339c6e8f8904fd921e575ffe028c677894421
Scarlett7-code <cs.zsuzsi333@gmail.com>
2025-01-22 23:17:29 +0100

    Update grid and list view

--- frontend/src/components/products/ProductsGrid.jsx
+++ frontend/src/components/products/ProductsGrid.jsx
@@ -1,45 +1,49 @@
-import React from 'react'
-import { FiShoppingCart } from 'react-icons/fi'
-import { LuHeart } from 'react-icons/lu'
+import React from 'react';
+import { FiShoppingCart } from 'react-icons/fi';
+import { LuHeart } from 'react-icons/lu';
+import { Link } from 'react-router-dom';
 
-import RatingStars from './RatingStars'
+import RatingStars from './RatingStars';
 
-export default function ProductsGrid({productsByCategory }) {
+export default function ProductsGrid({ productsByCategory }) {
   return (
     <div className="flex flex-wrap gap-8 justify-between md:mr-44">
-            {productsByCategory && productsByCategory.length > 0 ? (
-              productsByCategory.map((product, index) => (
-                <div>
-                  <div className="flex flex-col gap-2" key={index}>
-                    <div className="relative">
-                      <img
-                        className="border-2 rounded-2xl w-80 h-80 p-7 pr-8 shrink-0"
-                        src={product.pictureUrl}
-                        alt={product.name}
-                      />
-                      <button
-                        type="submit"
-                        className="absolute top-2 right-2 rounded-full flex items-center justify-center hover:text-primary"
-                      >
-                        <LuHeart className="m-2" />
-                      </button>
-                      <button
-                        type="submit"
-                        className="absolute top-9 right-2 rounded-full flex items-center justify-center hover:text-primary"
-                      >
-                        <FiShoppingCart className="m-2" />
-                      </button>
-                    </div>
-                    <div className="w-60 font-medium">{product.name}</div>
-                    <div className="font-medium text-lg">€{product.price}</div>
-                    <div className="flex gap-2 pb-2"><RatingStars rating={product.rating}/></div>
-                  </div>
-                </div>
-              ))
-            ) : (
-              <div>No products found for this category.</div>
-            )}
+      {productsByCategory && productsByCategory.length > 0 ? (
+        productsByCategory.map((product, index) => (
+          <div>
+            <div className="flex flex-col gap-2" key={index}>
+              <div className="relative">
+                <Link to={`/products/${product.id}`}>
+                  <img
+                    className="border-2 rounded-2xl w-80 h-80 p-7 pr-8 shrink-0 hover:border-gray-900"
+                    src={product.pictureUrl}
+                    alt={product.name}
+                  />
+                </Link>
+                <button
+                  type="submit"
+                  className="absolute top-2 right-2 rounded-full flex items-center justify-center hover:text-primary"
+                >
+                  <LuHeart className="m-2" />
+                </button>
+                <button
+                  type="submit"
+                  className="absolute top-9 right-2 rounded-full flex items-center justify-center hover:text-primary"
+                >
+                  <FiShoppingCart className="m-2" />
+                </button>
+              </div>
+              <div className="w-60 font-medium">{product.name}</div>
+              <div className="font-medium text-lg">€{product.price}</div>
+              <div className="flex gap-2 pb-2">
+                <RatingStars rating={product.rating} />
+              </div>
+            </div>
           </div>
-  )
+        ))
+      ) : (
+        <div>No products found for this category.</div>
+      )}
+    </div>
+  );
 }
-
--- frontend/src/components/products/ProductsList.jsx
+++ frontend/src/components/products/ProductsList.jsx
@@ -1,5 +1,6 @@
 import { FiShoppingCart } from 'react-icons/fi';
 import { LuHeart } from 'react-icons/lu';
+import { Link } from 'react-router-dom';
 
 import RatingStars from './RatingStars';
 
@@ -11,28 +12,35 @@ export default function ProductsList({ productsByCategory }) {
           <div>
             <div className="flex gap-12 " key={index}>
               <div className="relative">
-                <img
-                  className="border-2 rounded-2xl w-80 h-80 p-7 pr-8 shrink-0"
-                  src={product.pictureUrl}
-                  alt={product.name}
-                />
+                <Link to={`/products/${product.id}`}>
+                  <img
+                    className="border-2 rounded-2xl w-80 h-80 p-7 pr-8 shrink-0 hover:border-gray-900"
+                    src={product.pictureUrl}
+                    alt={product.name}
+                  />
+                </Link>
               </div>
               <div className="flex flex-col w-1/2 gap-3 justify-center">
                 <div className="text-lg font-medium">{product.name}</div>
                 <div className="text-justify">{product.description}</div>
                 <div className="font-medium text-lg pt-3 pb-3">€{product.price}</div>
-                <div className="flex gap-2 pb-2"><RatingStars rating={product.rating}/></div>
+                <div className="flex gap-2 pb-2">
+                  <RatingStars rating={product.rating} />
+                </div>
                 <div className="flex gap-4">
                   <div className="flex">
                     <button
                       type="button"
-                      className="flex items-center justify-center gap-3 w-40 rounded-xl bg-primary p-2 text-white  hover:text-black hover:font-semibold"
+                      className="flex items-center justify-center gap-3 w-40 rounded-xl border-2 border-primary bg-primary p-2 text-white hover:border-gray-900 hover:text-black"
                     >
                       <FiShoppingCart />
                       Add to Cart
                     </button>
                   </div>
-                  <button type="button" className="flex items-center text-xl justify-center p-1 hover:text-primary">
+                  <button
+                    type="button"
+                    className="flex items-center text-xl justify-center p-1 hover:text-primary"
+                  >
                     <LuHeart className="m-2" />
                   </button>
                 </div>
