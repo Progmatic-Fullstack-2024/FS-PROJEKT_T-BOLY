f564cc341b12856e797a9c45e7816b3df4c699d0
rekapuskas <166331809+rekapuskas@users.noreply.github.com>
2025-01-24 12:30:20 +0100

    fix responsive design

--- frontend/src/components/admin/AddNewUserModal.jsx
+++ frontend/src/components/admin/AddNewUserModal.jsx
@@ -37,85 +37,71 @@ export default function AddNewUserModal({ setIsOpen }) {
     }
   };
   return (
-    <div>
-      <div className="fixed inset-0 bg-gray-800 bg-opacity-50 flex justify-center items-center z-50">
-        <div className="bg-white rounded-lg shadow-lg p-6 w-full max-w-screen-lg">
-          <div className="flex justify-between items-center mb-4">
-            <h2 className="text-lg font-bold text-orange-500">Create new USER</h2>
+    <div className="fixed inset-0 bg-gray-800 bg-opacity-50 flex justify-center items-center z-50 ">
+      <div className="bg-white rounded-lg shadow-lg p-6 w-96 ">
+        <div className="flex justify-between items-center mb-4">
+          <h2 className="text-lg font-bold text-orange-500">Create new USER</h2>
 
-            <button
-              type="button"
-              className="text-gray-500 hover:text-black"
-              onClick={() => setIsOpen(false)}
-            >
-              ✖
-            </button>
-          </div>
+          <button
+            type="button"
+            className="text-gray-500 hover:text-black"
+            onClick={() => setIsOpen(false)}
+          >
+            ✖
+          </button>
+        </div>
 
-          <Formik initialValues={initialValues} onSubmit={handleCreate}>
-            {({ setFieldValue }) => (
-              <Form className="space-y-4">
-                <div className="flex justify-between items-center mb-4">
-                  <div className="flex flex-col w-2/3">
-                    <div>
-                      <label className="block text-left text-sm font-medium">First Name</label>
-                      <Field name="firstName" className="w-full p-2 border rounded-lg" />
-                      <ErrorMessage
-                        name="firstName"
-                        component="div"
-                        className="text-red-500 text-sm"
-                      />
-                    </div>
-
-                    <div>
-                      <label className="block text-left text-sm font-medium">Last Name</label>
-                      <Field name="lastName" className="w-full p-2 border rounded-lg" />
-                      <ErrorMessage
-                        name="lastName"
-                        component="div"
-                        className="text-red-500 text-sm"
-                      />
-                    </div>
-
-                    <div>
-                      <label className="block text-left text-sm font-medium">Email</label>
-                      <Field name="email" className="w-full p-2 border rounded-lg" />
-                      <ErrorMessage name="email" component="div" className="text-red-500 text-sm" />
-                    </div>
-
-                    <div>
-                      <label className="block text-left text-sm font-medium">Username</label>
-                      <Field name="username" className="w-full p-2 border rounded-lg" />
-                      <ErrorMessage
-                        name="username"
-                        component="div"
-                        className="text-red-500 text-sm"
-                      />
-                    </div>
-                  </div>
+        <Formik initialValues={initialValues} onSubmit={handleCreate}>
+          {({ setFieldValue }) => (
+            <Form className="flex flex-row w-full">
+              <div className="flex flex-col justify-between items-center mb-4 w-full">
+                <div className="w-full">
+                  <label className="block text-left text-sm font-medium  w-full">First Name</label>
+                  <Field name="firstName" className="block w-full p-2 border rounded-lg " />
+                  <ErrorMessage name="firstName" component="div" className="text-red-500 text-sm" />
                 </div>
-                <div>
+
+                <div className="w-full">
+                  <label className="block text-left text-sm font-medium">Last Name</label>
+                  <Field name="lastName" className="w-full p-2 border rounded-lg" />
+                  <ErrorMessage name="lastName" component="div" className="text-red-500 text-sm" />
+                </div>
+
+                <div className="w-full">
+                  <label className="block text-left text-sm font-medium">Email</label>
+                  <Field name="email" className="w-full p-2 border rounded-lg" />
+                  <ErrorMessage name="email" component="div" className="text-red-500 text-sm" />
+                </div>
+
+                <div className="w-full">
+                  <label className="block text-left text-sm font-medium">Username</label>
+                  <Field name="username" className="w-full p-2 border rounded-lg" />
+                  <ErrorMessage name="username" component="div" className="text-red-500 text-sm" />
+                </div>
+                <div className="w-full  mb-4">
                   <label className="block text-left text-sm font-medium">Role</label>
 
                   <Select
                     defaultValue={initialValues.role}
                     name="colors"
                     options={roleOptions}
-                    className="basic-select"
+                    className="blockya basic-select"
                     classNamePrefix="select"
                     onChange={(selectedOptions) => setFieldValue('role', selectedOptions)}
                   />
 
                   <ErrorMessage name="role" component="div" className="text-red-500 text-sm" />
                 </div>
-
-                <button type="submit" className="bg-primary text-white w-full py-2 rounded-lg">
-                  Create a new GAME
+                <button
+                  type="submit"
+                  className="block bg-primary text-white w-full  p-2 rounded-lg"
+                >
+                  Create a new USER
                 </button>
-              </Form>
-            )}
-          </Formik>
-        </div>
+              </div>
+            </Form>
+          )}
+        </Formik>
       </div>
     </div>
   );
