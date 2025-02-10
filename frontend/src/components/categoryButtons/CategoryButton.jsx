03d62504f691901701658bf6df4d757b38190870
rekapuskas <166331809+rekapuskas@users.noreply.github.com>
2025-02-10 15:16:45 +0100

    add images to clouds

--- frontend/src/components/categoryButtons/CategoryButton.jsx
+++ frontend/src/components/categoryButtons/CategoryButton.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-import VectorCloud1 from '../../assets/VectorCloud1.png';
-import VectorCloud2 from '../../assets/VectorCloud2.png';
-import VectorCloud3 from '../../assets/VectorCloud3.png';
-import VectorCloud4 from '../../assets/VectorCloud4.png';
-import VectorCloud5 from '../../assets/VectorCloud5.png';
+import VectorCloud1 from '../../assets/card-games.png';
+import VectorCloud2 from '../../assets/family-games.png';
+import VectorCloud3 from '../../assets/kid-games.png';
+import VectorCloud4 from '../../assets/lego-games.png';
+import VectorCloud5 from '../../assets/logic-games.png';
 
 const cloudImages = [VectorCloud1, VectorCloud2, VectorCloud3, VectorCloud4, VectorCloud5];
 export default function CategoryButton({ category, idx }) {
@@ -13,7 +13,7 @@ export default function CategoryButton({ category, idx }) {
     <Link className="w-40 flex flex-col items-center" to={`/products/category/${category.id}`}>
       <div>
         <img
-          className="w-32 h-32 object-contain"
+          className="w-48 h-48 object-contain"
           src={category.imageUrl || cloudImages[idx % cloudImages.length]}
           alt={category.name}
         />
