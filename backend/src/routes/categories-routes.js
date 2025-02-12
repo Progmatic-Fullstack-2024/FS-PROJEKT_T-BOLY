286f1f0c5fa3fb21012126b4fb5d3bf097883b17
vjanos1030 <vantaljanos1030@gmail.com>
2025-02-12 20:21:47 +0100

    Update categories rout on backend

--- backend/src/routes/categories-routes.js
+++ backend/src/routes/categories-routes.js
@@ -1,12 +1,13 @@
 import express from "express";
 import categoriesController from "../controllers/categories-conroller.js";
+import { upload } from "../middlewares/multer.js";
 
 const router = express.Router();
 
 router.get("/", categoriesController.getAllCategories);
 router.get("/:id", categoriesController.getCategoryById);
-router.post("/", categoriesController.createCategory);
-router.put("/:id", categoriesController.updateCategory);
+router.post("/", upload.single("image"), categoriesController.createCategory);
+router.put("/:id", upload.single("image"), categoriesController.updateCategory);
 router.delete("/:id", categoriesController.destroyCategory);
 
 export default router;
