097f148d262132cb8ee837f94b1c812727d3d4f6
Dániel Zoltán <102678885+szimplexum@users.noreply.github.com>
2025-02-13 18:02:28 +0100

    Payment / Card data collector DIV creation

---
+++ frontend/src/validations/payment.validation.js
@@ -0,0 +1,13 @@
+import * as yup from 'yup';
+
+export const paymentValidationSchema = yup.object({
+//   creditcard: yup.string().required('Card type is required'), //Checkbox
+  cardNumber: yup.string().label("Card number").min(16).max(16).typeError('Input must be a number!').required(),
+
+  nameOnCard: yup.string().required('Owner name is required'),
+
+  expirationDate: yup.string().min(6).max(9).required('Expiration date is required'),
+  securityCode: yup.number().required('CVC is required'),
+});
+
+export const validation = '';
